e665d938de5738296d5de852f639ff96a7317bed
RobiPoire <54003934+RobiPoire@users.noreply.github.com>
2025-08-17 11:03:32 +0200

    🎉 Initialize Fabric mod project in Kotlin

---
+++ src/client/kotlin/net/pearadise/pearexpansion/client/PearExpansionClient.kt
@@ -0,0 +1,9 @@
+package net.pearadise.pearexpansion.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class PearExpansionClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/net/pearadise/pearexpansion/PearExpansion.kt
@@ -0,0 +1,9 @@
+package net.pearadise.pearexpansion
+
+import net.fabricmc.api.ModInitializer
+
+class PearExpansion : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
